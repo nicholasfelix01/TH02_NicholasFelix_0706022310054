e5cca84a7b05a3e3f6c5f7dd7094b27b21ba0903
nicholasfelix01 <161397301+nicholasfelix01@users.noreply.github.com>
2024-03-06 01:00:49 +0700

    Add files via upload

---
+++ TH0202_NF/TH0202_NF/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace TH0202_NF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl_01 = new Label();
+            tb_01 = new TextBox();
+            tb_02 = new TextBox();
+            lbl_02 = new Label();
+            tb_03 = new TextBox();
+            lbl_03 = new Label();
+            tb_04 = new TextBox();
+            lbl_04 = new Label();
+            tb_05 = new TextBox();
+            lbl_05 = new Label();
+            lbl_06 = new Label();
+            lbl_07 = new Label();
+            button1 = new Button();
+            lbl_08 = new Label();
+            lbl_09 = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // lbl_01
+            // 
+            lbl_01.AutoSize = true;
+            lbl_01.Location = new Point(44, 60);
+            lbl_01.Name = "lbl_01";
+            lbl_01.Size = new Size(76, 20);
+            lbl_01.TabIndex = 0;
+            lbl_01.Text = "First Word";
+            // 
+            // tb_01
+            // 
+            tb_01.Location = new Point(126, 57);
+            tb_01.Name = "tb_01";
+            tb_01.Size = new Size(125, 27);
+            tb_01.TabIndex = 1;
+            tb_01.TextChanged += tb_01_TextChanged;
+            // 
+            // tb_02
+            // 
+            tb_02.Location = new Point(126, 90);
+            tb_02.Name = "tb_02";
+            tb_02.Size = new Size(125, 27);
+            tb_02.TabIndex = 3;
+            tb_02.TextChanged += tb_02_TextChanged;
+            // 
+            // lbl_02
+            // 
+            lbl_02.AutoSize = true;
+            lbl_02.Location = new Point(22, 93);
+            lbl_02.Name = "lbl_02";
+            lbl_02.Size = new Size(98, 20);
+            lbl_02.TabIndex = 2;
+            lbl_02.Text = "Second Word";
+            // 
+            // tb_03
+            // 
+            tb_03.Location = new Point(126, 123);
+            tb_03.Name = "tb_03";
+            tb_03.Size = new Size(125, 27);
+            tb_03.TabIndex = 5;
+            tb_03.TextChanged += tb_03_TextChanged;
+            // 
+            // lbl_03
+            // 
+            lbl_03.AutoSize = true;
+            lbl_03.Location = new Point(37, 126);
+            lbl_03.Name = "lbl_03";
+            lbl_03.Size = new Size(83, 20);
+            lbl_03.TabIndex = 4;
+            lbl_03.Text = "Third Word";
+            // 
+            // tb_04
+            // 
+            tb_04.Location = new Point(126, 156);
+            tb_04.Name = "tb_04";
+            tb_04.Size = new Size(125, 27);
+            tb_04.TabIndex = 7;
+            tb_04.TextChanged += tb_04_TextChanged;
+            // 
+            // lbl_04
+            // 
+            lbl_04.AutoSize = true;
+            lbl_04.Location = new Point(29, 159);
+            lbl_04.Name = "lbl_04";
+            lbl_04.Size = new Size(91, 20);
+            lbl_04.TabIndex = 6;
+            lbl_04.Text = "Fourth Word";
+            // 
+            // tb_05
+            // 
+            tb_05.Location = new Point(126, 189);
+            tb_05.Name = "tb_05";
+            tb_05.Size = new Size(125, 27);
+            tb_05.TabIndex = 9;
+            tb_05.TextChanged += tb_05_TextChanged;
+            // 
+            // lbl_05
+            // 
+            lbl_05.AutoSize = true;
+            lbl_05.Location = new Point(42, 192);
+            lbl_05.Name = "lbl_05";
+            lbl_05.Size = new Size(78, 20);
+            lbl_05.TabIndex = 8;
+            lbl_05.Text = "Fifth Word";
+            // 
+            // lbl_06
+            // 
+            lbl_06.AutoSize = true;
+            lbl_06.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_06.Location = new Point(364, 9);
+            lbl_06.Name = "lbl_06";
+            lbl_06.Size = new Size(66, 35);
+            lbl_06.TabIndex = 10;
+            lbl_06.Text = "Rules";
+            // 
+            // lbl_07
+            // 
+            lbl_07.AutoSize = true;
+            lbl_07.Font = new Font("Segoe Print", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_07.Location = new Point(270, 64);
+            lbl_07.Name = "lbl_07";
+            lbl_07.Size = new Size(256, 23);
+            lbl_07.TabIndex = 11;
+            lbl_07.Text = "* None of the words can be identical";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(126, 231);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 42);
+            button1.TabIndex = 12;
+            button1.Text = "Play";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // lbl_08
+            // 
+            lbl_08.AutoSize = true;
+            lbl_08.Font = new Font("Segoe Print", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_08.Location = new Point(270, 94);
+            lbl_08.Name = "lbl_08";
+            lbl_08.Size = new Size(478, 23);
+            lbl_08.TabIndex = 13;
+            lbl_08.Text = "* Game cannot be started unless all 5 TextBox are filled with a word.";
+            // 
+            // lbl_09
+            // 
+            lbl_09.AutoSize = true;
+            lbl_09.Font = new Font("Segoe Print", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_09.Location = new Point(270, 123);
+            lbl_09.Name = "lbl_09";
+            lbl_09.Size = new Size(452, 23);
+            lbl_09.TabIndex = 14;
+            lbl_09.Text = "* Words must be 5 letters, and cannot go beyond nor less of that.";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Print", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(270, 156);
+            label1.Name = "label1";
+            label1.Size = new Size(359, 23);
+            label1.TabIndex = 15;
+            label1.Text = "* Words must be not contain Symbols nor Numbers.";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(761, 306);
+            Controls.Add(label1);
+            Controls.Add(lbl_09);
+            Controls.Add(lbl_08);
+            Controls.Add(button1);
+            Controls.Add(lbl_07);
+            Controls.Add(lbl_06);
+            Controls.Add(tb_05);
+            Controls.Add(lbl_05);
+            Controls.Add(tb_04);
+            Controls.Add(lbl_04);
+            Controls.Add(tb_03);
+            Controls.Add(lbl_03);
+            Controls.Add(tb_02);
+            Controls.Add(lbl_02);
+            Controls.Add(tb_01);
+            Controls.Add(lbl_01);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_01;
+        private TextBox tb_01;
+        private TextBox tb_02;
+        private Label lbl_02;
+        private TextBox tb_03;
+        private Label lbl_03;
+        private TextBox tb_04;
+        private Label lbl_04;
+        private TextBox tb_05;
+        private Label lbl_05;
+        private Label lbl_06;
+        private Label lbl_07;
+        private Button button1;
+        private Label lbl_08;
+        private Label lbl_09;
+        private Label label1;
+    }
+}
---
+++ TH0202_NF/TH0202_NF/Form2.cs
@@ -0,0 +1,703 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TH0202_NF
+{
+    public partial class Form2 : Form
+    {
+        bool[] katabetul = new bool[5];
+        char[] woword = new char[5];
+        public Form2(string[] word)
+        {
+            InitializeComponent();
+            Random random = new Random();
+            int angks = random.Next(0, 5);
+            string theWord = word[angks];
+            label1.Text = theWord;
+            int huhu = 0;
+            foreach (char c in theWord)
+            {
+                woword[huhu] = c;
+                huhu++;
+            }
+        }
+
+        private void didweWIN()
+        {
+            int hoahao = 0;
+            foreach (bool b in katabetul)
+            {
+                if (b)
+                {
+                    hoahao++;
+                }
+            }
+            if (hoahao == 5)
+            {
+                MessageBox.Show("You Won.");
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_A_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'A')
+                {
+                    Controls[$"lbl_{i}"].Text = "A";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_A.BackColor = Color.Green;
+                btn_A.Enabled = false;
+            }
+            else
+            {
+                btn_A.BackColor = Color.Red;
+                btn_A.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_B_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'B')
+                {
+                    Controls[$"lbl_{i}"].Text = "B";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_B.BackColor = Color.Green;
+                btn_B.Enabled = false;
+            }
+            else
+            {
+                btn_B.BackColor = Color.Red;
+                btn_B.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_C_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'C')
+                {
+                    Controls[$"lbl_{i}"].Text = "C";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_C.BackColor = Color.Green;
+                btn_C.Enabled = false;
+            }
+            else
+            {
+                btn_C.BackColor = Color.Red;
+                btn_C.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_D_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'D')
+                {
+                    Controls[$"lbl_{i}"].Text = "D";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_D.BackColor = Color.Green;
+                btn_D.Enabled = false;
+            }
+            else
+            {
+                btn_D.BackColor = Color.Red;
+                btn_D.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_E_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'E')
+                {
+                    Controls[$"lbl_{i}"].Text = "E";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_E.BackColor = Color.Green;
+                btn_E.Enabled = false;
+            }
+            else
+            {
+                btn_E.BackColor = Color.Red;
+                btn_E.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_F_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'F')
+                {
+                    Controls[$"lbl_{i}"].Text = "F";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_F.BackColor = Color.Green;
+                btn_F.Enabled = false;
+            }
+            else
+            {
+                btn_F.BackColor = Color.Red;
+                btn_F.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_G_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'G')
+                {
+                    Controls[$"lbl_{i}"].Text = "G";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_G.BackColor = Color.Green;
+                btn_G.Enabled = false;
+            }
+            else
+            {
+                btn_G.BackColor = Color.Red;
+                btn_G.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_H_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'H')
+                {
+                    Controls[$"lbl_{i}"].Text = "H";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_H.BackColor = Color.Green;
+                btn_H.Enabled = false;
+            }
+            else
+            {
+                btn_H.BackColor = Color.Red;
+                btn_H.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_I_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'I')
+                {
+                    Controls[$"lbl_{i}"].Text = "I";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_I.BackColor = Color.Green;
+                btn_I.Enabled = false;
+            }
+            else
+            {
+                btn_I.BackColor = Color.Red;
+                btn_I.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_J_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'J')
+                {
+                    Controls[$"lbl_{i}"].Text = "J";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_J.BackColor = Color.Green;
+                btn_J.Enabled = false;
+            }
+            else
+            {
+                btn_J.BackColor = Color.Red;
+                btn_J.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_K_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'K')
+                {
+                    Controls[$"lbl_{i}"].Text = "K";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_K.BackColor = Color.Green;
+                btn_K.Enabled = false;
+            }
+            else
+            {
+                btn_K.BackColor = Color.Red;
+                btn_K.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_L_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'L')
+                {
+                    Controls[$"lbl_{i}"].Text = "L";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_L.BackColor = Color.Green;
+                btn_L.Enabled = false;
+            }
+            else
+            {
+                btn_L.BackColor = Color.Red;
+                btn_L.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_M_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'M')
+                {
+                    Controls[$"lbl_{i}"].Text = "M";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_M.BackColor = Color.Green;
+                btn_M.Enabled = false;
+            }
+            else
+            {
+                btn_M.BackColor = Color.Red;
+                btn_M.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_N_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'N')
+                {
+                    Controls[$"lbl_{i}"].Text = "N";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_N.BackColor = Color.Green;
+                btn_N.Enabled = false;
+            }
+            else
+            {
+                btn_N.BackColor = Color.Red;
+                btn_N.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_O_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'O')
+                {
+                    Controls[$"lbl_{i}"].Text = "O";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_O.BackColor = Color.Green;
+                btn_O.Enabled = false;
+            }
+            else
+            {
+                btn_O.BackColor = Color.Red;
+                btn_O.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_P_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'P')
+                {
+                    Controls[$"lbl_{i}"].Text = "P";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_P.BackColor = Color.Green;
+                btn_P.Enabled = false;
+            }
+            else
+            {
+                btn_P.BackColor = Color.Red;
+                btn_P.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_Q_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'Q')
+                {
+                    Controls[$"lbl_{i}"].Text = "Q";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_Q.BackColor = Color.Green;
+                btn_Q.Enabled = false;
+            }
+            else
+            {
+                btn_Q.BackColor = Color.Red;
+                btn_Q.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_R_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'R')
+                {
+                    Controls[$"lbl_{i}"].Text = "R";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_R.BackColor = Color.Green;
+                btn_R.Enabled = false;
+            }
+            else
+            {
+                btn_R.BackColor = Color.Red;
+                btn_R.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_S_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'S')
+                {
+                    Controls[$"lbl_{i}"].Text = "S";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_S.BackColor = Color.Green;
+                btn_S.Enabled = false;
+            }
+            else
+            {
+                btn_S.BackColor = Color.Red;
+                btn_S.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_T_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'T')
+                {
+                    Controls[$"lbl_{i}"].Text = "T";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_T.BackColor = Color.Green;
+                btn_T.Enabled = false;
+            }
+            else
+            {
+                btn_T.BackColor = Color.Red;
+                btn_T.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_U_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'U')
+                {
+                    Controls[$"lbl_{i}"].Text = "U";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_U.BackColor = Color.Green;
+                btn_U.Enabled = false;
+            }
+            else
+            {
+                btn_U.BackColor = Color.Red;
+                btn_U.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_V_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'V')
+                {
+                    Controls[$"lbl_{i}"].Text = "V";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_V.BackColor = Color.Green;
+                btn_V.Enabled = false;
+            }
+            else
+            {
+                btn_V.BackColor = Color.Red;
+                btn_V.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_W_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'W')
+                {
+                    Controls[$"lbl_{i}"].Text = "W";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_W.BackColor = Color.Green;
+                btn_W.Enabled = false;
+            }
+            else
+            {
+                btn_W.BackColor = Color.Red;
+                btn_W.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_X_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'X')
+                {
+                    Controls[$"lbl_{i}"].Text = "X";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_X.BackColor = Color.Green;
+                btn_X.Enabled = false;
+            }
+            else
+            {
+                btn_X.BackColor = Color.Red;
+                btn_X.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_Y_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'Y')
+                {
+                    Controls[$"lbl_{i}"].Text = "Y";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_Y.BackColor = Color.Green;
+                btn_Y.Enabled = false;
+            }
+            else
+            {
+                btn_Y.BackColor = Color.Red;
+                btn_Y.Enabled = false;
+            }
+            didweWIN();
+        }
+
+        private void btn_Z_Click(object sender, EventArgs e)
+        {
+            bool hohoho = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (woword[i] == 'Z')
+                {
+                    Controls[$"lbl_{i}"].Text = "Z";
+                    katabetul[i] = true;
+                    hohoho = true;
+                }
+            }
+            if (hohoho)
+            {
+                btn_Z.BackColor = Color.Green;
+                btn_Z.Enabled = false;
+            }
+            else
+            {
+                btn_Z.BackColor = Color.Red;
+                btn_Z.Enabled = false;
+            }
+            didweWIN();
+        }
+    }
+}
